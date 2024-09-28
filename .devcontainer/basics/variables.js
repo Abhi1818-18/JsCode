9d3c15a58f2fea1b9ea03bd429b0775b2f2514ab
Abhi1818-18 <131249108+Abhi1818-18@users.noreply.github.com>
2024-09-28 05:03:33 +0000

    variables file added

---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,6 @@
+const accountId = 34244;
+let accountEmail = "abhi@gmail.com";
+var accountPassword = "12345";
+accountCity = "pune"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
